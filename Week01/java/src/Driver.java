090afbdb613dd84f6447afbf181f47c712fb5299
ReeseHatfield <reesehatfield02@gmail.com>
2026-08-26 15:33:15 -0400

    aug 26th

---
+++ Week01/java/src/Driver.java
@@ -0,0 +1,13 @@
+public class Driver {
+    
+    
+    public static void main(String[] args){
+        int result = add(1, 2);
+        System.out.println("Result was: " 
+        + result);
+    }
+    
+    public static int add(int a, int b){
+        return a + b;
+    }
+}
---
+++ Week01/python/main.py
@@ -0,0 +1,29 @@
+print("hello world")
+
+# type name = assignemnt
+
+
+result = add(3, 4)
+print(f"Result: {result}")
+
+def add(a, b):
+    return a + b
+
+
+string_restult = add("Hello", "World")
+print(f"Result string {string_restult}")
+
+print()
+print()
+print()
+
+a = 5
+a = "Hello"
+
+first_name = "John"
+last_name = "Johnson"
+name = f"{first_name} {last_name}"
+print(f"Hello, {name}")
+
+print(a)
+print("hello world 2")

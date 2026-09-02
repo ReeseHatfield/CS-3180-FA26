90730f39e0027622a32b1f25ce6b1ce6317fe261
ReeseHatfield <reesehatfield02@gmail.com>
2026-09-02 15:46:42 -0400

    sep 2nd

--- Week02/java/src/Driver.java
+++ Week02/java/src/Driver.java
@@ -3,8 +3,25 @@ public class Driver {
         // int double, char, byte
 
         String a = "";
+
         int b = 5;
         // /way later on
 
+        // widden this type
+        // without in
+        double d = 5;
+
+        // narrowed
+        // opt into that
+        int c = (int) 5.5;
+
+        var v = new Integer(5);
+
+
+        // greet();
+    }
+
+    public static void greet(String s){
+        System.out.println("Hello " + s);
     }
 }
